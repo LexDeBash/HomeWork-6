37d7d753753c28f635089a64e95b5e3d2307216e
Алексей Ефимов <lex.efimov@gmail.com>
2018-06-17 14:51:03 +0500

    Initial Commit

---
+++ HW3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW3
+//
+//  Created by Alexey Efimov on 17.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
