8e1ec2cd7f3125bdf946d9dc54a81ed26cb953b3
Alexey Efimov <lex.efimov@gmail.com>
2020-06-10 17:13:33 +0500

    from var to let

--- HW3/DataManager.swift
+++ HW3/DataManager.swift
@@ -14,20 +14,20 @@ class DataManager {
     
     private init() {}
     
-    var names = ["John", "Aaron", "Tim", "Ted",
+    let names = ["John", "Aaron", "Tim", "Ted",
                  "Steven", "Sharon", "Nicola",
                  "Allan", "Bruce", "Carl"]
     
-    var surnames = ["Smith", "Dow", "Isaacson", "Pennyworth",
+    let surnames = ["Smith", "Dow", "Isaacson", "Pennyworth",
                     "Jankin", "Butler", "Black", "Robertson",
                     "Murphy", "Williams"]
     
-    var emails = ["jjjj@mail.ru", "aaaa@mail.ru", "eeee@mail.ru",
+    let emails = ["jjjj@mail.ru", "aaaa@mail.ru", "eeee@mail.ru",
                   "hhhh@mail.ru", "wwww@mail.ru", "mmmm@mail.ru",
                   "xxxx@mail.ru", "pppp@mail.ru", "ssss@mail.ru",
                   "llll@mail.ru"]
     
-    var phones = ["745396026", "145036843", "287185656", "293520954",
+    let phones = ["745396026", "145036843", "287185656", "293520954",
                   "562880225", "738594072", "825654134", "645898473",
                   "432349845", "459477625"]
 }
